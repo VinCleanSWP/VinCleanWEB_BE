68a0da0b6f01f621dfc3e38de9ac4a323312adce
N-RjD <119560093+N-RjD@users.noreply.github.com>
2023-06-28 18:36:35 +0700

    Update CRUD Rating

---
+++ BackEnd/VinClean.Repo/Models/RatingModelDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinClean.Repo.Models
+{
+    public class RatingModelDTO
+    {
+        public int RateId { get; set; }
+        public int OrderId { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceType { get; set; }
+        public string Note { get; set; }
+        public string CustomerName { get; set; }
+        public byte? Rate { get; set; }
+        public string Comment { get; set; }
+        public DateTime? RatedDate { get; set; }
+    }
+}
